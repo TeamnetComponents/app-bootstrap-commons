78a0e59851b9a72fefa3497964afa6311187af19
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2015-07-07 10:44:06 +0300

    refactor jpa package to scan

---
+++ src/main/java/ro/teamnet/bootstrap/plugin/jpa/JpaPackagesToScanPlugin.java
@@ -0,0 +1,13 @@
+package ro.teamnet.bootstrap.plugin.jpa;
+
+import org.springframework.plugin.core.Plugin;
+
+import java.util.List;
+
+public interface JpaPackagesToScanPlugin extends Plugin<JpaType> {
+
+    public List<String> packagesToScan();
+
+    public String packageToScan();
+
+}
---
+++ src/main/java/ro/teamnet/bootstrap/plugin/security/SecurityType.java
@@ -0,0 +1,10 @@
+package ro.teamnet.bootstrap.plugin.security;
+
+public enum SecurityType {
+
+    //using for register a plugin for user authentication service
+    USER_AUTHENTICATION,
+
+    //using to register a plugin for user authorization service
+    USER_AUTHORIZATION,
+}
